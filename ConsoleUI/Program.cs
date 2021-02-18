adcbc26e78855acc53051946646a49e3511b527e
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-02-18 20:01:05 +0300

    18 şubat

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleUI
             foreach (var car in carManager.GetAll())
             {
                     Console.WriteLine(car.BrandId);
+                    Console.WriteLine(car.BrandId);
             }
             
         }
