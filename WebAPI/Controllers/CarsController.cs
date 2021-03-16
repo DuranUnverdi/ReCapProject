c1fab454cc33cf3b0bd292a4529b44ea6f6037b8
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-03-16 20:54:53 +0300

    16 mart web api second commit

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -22,14 +22,37 @@ namespace WebAPI.Controllers
             _carService = carService;
         }
 
-        [HttpGet]
-        public List<Car> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
            
             var result =_carService.GetAll();
-            return result.Data;
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
-    
+        [HttpGet("getbrandid")]
+        public IActionResult GetBrandId(int id)
+        {
+            var result = _carService.GetAllByBrandId(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+                }
+    [HttpPost("add")]
+        public IActionResult Add(Car car)
+        {
+            var result = _carService.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
 
     }
 }
