c074c47ddc47bc366922f53d5b20aef9824dd742
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-03-20 19:38:49 +0300

    20 mart Autofac,IoC

---
+++ Business/ValidationRules/FluentVlidation/CarValidator.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentVlidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Description).MinimumLength(5);
+            RuleFor(c => c.Description).NotEmpty();
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(100).When(c => c.BrandId == 1);
+            RuleFor(c => c.Description).Must(StartWithA).WithMessage("Açıklama A harfi ile başlamalıdır");
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
